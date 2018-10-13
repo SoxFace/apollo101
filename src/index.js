649fd7fd236ba7cbc9af10864f021ed8d4579735
Sonya Corcoran <sonyacorcoran@gmail.com>
2018-10-13 19:40:22 +1100

    writing queries with dogs

--- src/index.js
+++ src/index.js
@@ -1,65 +1,96 @@
-import React from 'react';
-
+import React, { Component } from "react";
 import { render } from "react-dom";
-import './index.css';
-import * as serviceWorker from './serviceWorker';
-import { ApolloProvider } from "react-apollo";
-import ApolloClient from 'apollo-boost';
-import { Query } from "react-apollo";
+
+import ApolloClient from "apollo-boost";
+import { ApolloProvider, Query } from "react-apollo";
 import gql from "graphql-tag";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
-
 const client = new ApolloClient({
-  uri: "https://w5xlvm3vzz.lp.gql.zone/graphql"
+  uri: `https://nx9zvp49q7.lp.gql.zone/graphql`
 });
 
-client
-  .query({
-    query: gql`
-      {
-        rates(currency: "USD") {
-          currency
-        }
-      }
-      `
-  })
-  .then(result => console.log(result));
+const GET_DOGS = gql`
+  {
+    dogs {
+      id
+      breed
+    }
+  }
+`;
 
-const App = () => (
-  <ApolloProvider client={client}>
-    <div>
-      <h2>My first Apollo App <span role="img" alt="rocket">🚀</span></h2>
-      <ExchangeRates />
-    </div>
-    </ApolloProvider>
-);
-
-const ExchangeRates = () => (
-  <Query
-    query={gql`
-      {
-        rates(currency: "USD") {
-          currency
-          rate
-        }
-      }
-    `}
-  >
+const Dogs = ({ onDogSelected }) => (
+  <Query query={GET_DOGS}>
     {({ loading, error, data }) => {
-      if (loading) return <p>Loading...</p>;
-      if (error) return <p>Error :(</p>;
+      if (loading) return "Loading...";
+      if (error) return `Error! ${error.message}`;
 
-      return data.rates.map(({ currency, rate }) => (
-        <div key={currency}>
-          <p>{`${currency}: ${rate}`}</p>
-        </div>
-      ));
+      return (
+        <select name="dog" onChange={onDogSelected}>
+          {data.dogs.map(dog => (
+            <option key={dog.id} value={dog.breed}>
+              {dog.breed}
+            </option>
+          ))}
+        </select>
+      );
     }}
   </Query>
 );
+
+const GET_DOG_PHOTO = gql`
+  query dog($breed: String!) {
+    dog(breed: $breed) {
+      id
+      displayImage
+    }
+  }
+`;
+
+const DogPhoto = ({ breed }) => (
+  <Query
+    query={GET_DOG_PHOTO}
+    variables={{ breed }}
+    notifyOnNetworkStatusChange
+    // pollInterval={500}
+  >
+    {({ loading, error, data, refetch, networkStatus }) => {
+      if (networkStatus === 4) return "Refetching!";
+      if (loading) return null;
+      if (error) return `Error!: ${error}`;
+
+      return (
+        <div>
+          <img
+            src={data.dog.displayImage}
+            style={{ height: 100, width: 100 }}
+          />
+          <button onClick={() => refetch()}>Refetch!</button>
+        </div>
+      );
+    }}
+  </Query>
+);
+
+class App extends Component {
+  state = { selectedDog: null };
+
+  onDogSelected = ({ target }) => {
+    this.setState(() => ({ selectedDog: target.value }));
+  };
+
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div>
+          <h2>Building Query components 🚀</h2>
+          {this.state.selectedDog && (
+            <DogPhoto breed={this.state.selectedDog} />
+          )}
+          <Dogs onDogSelected={this.onDogSelected} />
+        </div>
+      </ApolloProvider>
+    );
+  }
+}
 
 render(<App />, document.getElementById("root"));
